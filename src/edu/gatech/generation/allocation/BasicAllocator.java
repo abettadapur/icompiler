6255452593600be92be636663eb558751bee3145
abettadapur <biscuit1>
2014-04-20 13:40:18 -0400

    statement insertion

--- src/edu/gatech/generation/allocation/BasicAllocator.java
+++ src/edu/gatech/generation/allocation/BasicAllocator.java
@@ -37,7 +37,7 @@ public class BasicAllocator implements IAllocator
     private void annotateBlock(BasicBlock b)
     {
         InterferenceGraph graph = new InterferenceGraph();
-        HashMap<String, Pair<Integer, Integer>> liveRangeTable = new HashMap<>();
+        HashMap<String, List<Pair<Integer, Integer>>> liveRangeTable = new HashMap<>();
         Set<String> buffer = new LinkedHashSet<>();
         //Set<String> dead = new LinkedHashSet<>();
         Set<String> prevIn = null;
@@ -53,28 +53,63 @@ public class BasicAllocator implements IAllocator
                     if(!s.equals(s2))
                         graph.addEdge(s, s2);
                 }
-                if(!liveRangeTable.containsKey(s))
+               /* if(!liveRangeTable.containsKey(s))
                 {
-                    liveRangeTable.put(s, new Pair<Integer,Integer>(i,-1));
-                }
+                    List<Pair<Integer,Integer>> ranges = new ArrayList<>();
+                    ranges.add(new Pair<Integer,Integer>(i,-1));
+                    liveRangeTable.put(s, ranges);
+                }*/
             }
+            Set<String> live = new HashSet<>();
             Set<String> dead = new HashSet<>();
             if(prevIn!=null)
+            {
                 dead.addAll(prevIn);
+            }
+            if(in!=null)
+            {
+                live.addAll(in);
+            }
             if(!dead.isEmpty())
             {
                 dead.removeAll(in);//dead now contains the variables that died in the last instruction
                 for(String s: dead)
                 {
                     if(liveRangeTable.containsKey(s))
-                        liveRangeTable.get(s).setU(i-1);
+                    {
+                        List<Pair<Integer,Integer>> ranges = liveRangeTable.get(s);
+                        ranges.get(ranges.size()-1).setU(i-1);
+                    }
+                }
+            }
+            if(!live.isEmpty())
+            {
+                if(prevIn!=null)
+                    live.removeAll(prevIn);
+                for(String s: live)
+                {
+                    List<Pair<Integer,Integer>> ranges = null;
+                    if(!liveRangeTable.containsKey(s))
+                    {
+                        ranges = new ArrayList<>();
+
+                    }
+                    else
+                    {
+                       ranges = liveRangeTable.get(s);
+                    }
+                    ranges.add(new Pair<Integer,Integer>(i,-1));
+                    liveRangeTable.put(s, ranges);
                 }
             }
 
             prevIn = in;
         }
         graph.colorGraph(5); //TODO: ABSTRACT
-        for(Map.Entry<String, Pair<Integer, Integer>> entry:liveRangeTable.entrySet())
+
+
+        List<Pair<Integer, IntermediateOperation>> toInsert = new ArrayList<>();
+        for(Map.Entry<String, List<Pair<Integer, Integer>>> entry:liveRangeTable.entrySet())
         {
             int color = graph.getColor(entry.getKey());
             int registerNumber = -1;
@@ -83,18 +118,52 @@ public class BasicAllocator implements IAllocator
 
             if(registerNumber!=-1)
             {
-                int loadIndex = entry.getValue().getT();
-                int storeIndex = entry.getValue().getU();
-
-                IntermediateOperation load = new IntermediateOperation(Operator.LOAD, "", "$"+registerNumber,entry.getKey(), "", null);
-                IntermediateOperation store = new IntermediateOperation(Operator.STORE, "",entry.getKey(),"$"+registerNumber, "", null);
-
-
+                for(Pair<Integer, Integer> pair:entry.getValue())
+                {
+                    IntermediateOperation load = new IntermediateOperation(Operator.LOAD, "$"+registerNumber,entry.getKey(), "","", null);
+                    IntermediateOperation store = new IntermediateOperation(Operator.STORE,entry.getKey(),"$"+registerNumber, "","", null);
+                    if(pair.getU()==-1)
+                        pair.setU(b.getContents().size()-1);
+                    toInsert.add(new Pair<>(pair.getT(), load));
+                    toInsert.add(new Pair<>(pair.getU(), store));
+                }
             }
-
-
-
+            else
+            {
+                //spill stuff
+            }
         }
+        Collections.sort(toInsert, new Comparator<Pair<Integer, IntermediateOperation>>() {
+            @Override
+            public int compare(Pair<Integer, IntermediateOperation> o1, Pair<Integer, IntermediateOperation> o2)
+            {
+                int diff = o1.getT() - o2.getT();
+                if(diff==0)
+                {
+                    if(o1.getU().getOp()==o2.getU().getOp())
+                        return 0;
+                    else if(o1.getU().getOp()==Operator.LOAD)
+                        return -1;
+                    else
+                        return 1;
+
+                }
+                return diff;
+            }
+        });
+
+        int offset = 0;
+        for(Pair<Integer, IntermediateOperation> op:toInsert)
+        {
+            if(op.getU().getOp()==Operator.LOAD)
+                b.getContents().add(op.getT()+offset, op.getU());
+            else
+                b.getContents().add(op.getT()+offset+1, op.getU());
+            offset++;
+        }
+
+
+
 
 
 
